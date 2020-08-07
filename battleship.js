f102ec391ccc10c1fa5ae93582f05baf184657e3
Route9Library <Jhmediaatworks@gmail.com>
2020-08-07 16:30:16 -0400

    almost done with the battleships

--- battleship.js
+++ battleship.js
@@ -1,17 +1,105 @@
 
 
-Math.floor(Math.random() * 22);     // returns a random integer from 0 to 21
+// Math.floor(Math.random() * 6);     // returns a random integer from 0 to 21
 
 
 
 
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// while (isSunk == false){
+//     guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
+// }
+
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// while (isSunk == false){
+//     guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
+// }
+
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// while (isSunk == false){
+//     guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
+
+//     if (guess < 0 || guess > 6) {
+//         alert("Please enter a valid cell number!");
+//     } else {
+//         guesses = guesses + 1; 
+//         }
+
+//     if (guess == location1 || guess == location2 || guess == location3) { 
+//             alert("HIT!");
+//             hits = hits + 1; 
+//                 if (hits == 3) {
+//                     isSunk = true;
+//                     alert("You sank my battleship!"); 
+//                 }
+//     }   else {
+//             alert("MISS");
+//          }
+// }
+
+
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSunk = false;
+
+// while (isSunk == false){
+//     guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
+
+//     if (guess < 0 || guess > 21) {
+//         alert("Please enter a valid cell number!");
+//     } else {
+//         guesses = guesses + 1; 
+//         }
+
+//     if (guess == location1 || guess == location2 || guess == location3) { 
+//             alert("HIT!");
+//             hits = hits + 1; 
+//                 if (hits == 3) {
+//                     isSunk = true;
+//                     alert("You sank my battleship!"); 
+//                 }
+//     }   else {
+//             alert("MISS");
+//          }
+// }
+// var stats = "You took " + guesses + " guesses to sink the battleship, " +
+//     "which means your shooting accuracy was " + (3/guesses); 
+//     alert(stats);
+
+
 
 var location1 = 3;
 var location2 = 4;
@@ -22,33 +110,9 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
-}
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
 
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
-while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
-}
-
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
-while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
-
-    if (guess < 0 || guess > 21) {
+    if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1; 
@@ -66,35 +130,6 @@ while (isSunk == false){
          }
 }
 
-
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
-while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-21):")
-
-    if (guess < 0 || guess > 21) {
-        alert("Please enter a valid cell number!");
-    } else {
-        guesses = guesses + 1; 
-        }
-
-    if (guess == location1 || guess == location2 || guess == location3) { 
-            alert("HIT!");
-            hits = hits + 1; 
-                if (hits == 3) {
-                    isSunk = true;
-                    alert("You sank my battleship!"); 
-                }
-    }   else {
-            alert("MISS");
-         }
-}
-var stats = "You took " + guesses + " guesses to sink the battleship, " +
-    "which means your shooting accuracy was " + (3/guesses); 
+    var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (1/guesses); 
     alert(stats);
