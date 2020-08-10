7025713e09834df0c0f81add76028c8caa721d91
Route9Library <Jhmediaatworks@gmail.com>
2020-08-10 13:34:21 -0400

    functions

---
+++ functions.js
@@ -0,0 +1,43 @@
+function multiplyFive(x){
+    return x*5;
+}
+
+// console.log(multiplyFive(100));
+
+
+function calculateDogAge(x){
+    var age = 7*x;
+        console.log("Your doggie is " + age + " years old in human years");
+return x * 7;
+}
+
+// console.log(calculateDogAge(2));
+
+function addition(x,y){
+return x + y;
+}
+
+// console.log(addition(23,6));
+
+function increment(x){
+    return x+1;
+}
+
+// console.log(increment(29));
+
+
+// function word(arr) {
+// var arr = [];
+// return arr[3];
+// }
+// var array = [11, "song", true];
+
+// console.log(word(11, "song", true));
+
+// function reminder(x , y
+//     return x % y;
+
+// )
+
+
+// console.log(remainder(847,29));
